bcb0f3973859289e24f8716959e207269f625e29
chart21 <chart21@web.de>
2025-08-15 19:04:41 +0200

    updated mp-spdz and put -R 32 as default option

--- host_scripts/mp-spdz/measurement.sh
+++ host_scripts/mp-spdz/measurement.sh
@@ -119,7 +119,7 @@ esac
 ####
 
 for protocol in "${protocols[@]}"; do
-    make -j "${protocol}" || { echo "Failed to compile $protocol"}
+    make -j "${protocol}" 
 
     log=testresults"$cdomain""${protocol::-8}"
     touch "$log"
